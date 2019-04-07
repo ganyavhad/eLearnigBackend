501884e42fe11e518f91a024d77e1f487dce8897
ganyavhad <36654727+ganyavhad@users.noreply.github.com>
2019-04-07 10:36:07 +0530

    new setup

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
-const routes: Routes =[
+const routes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard',
@@ -14,10 +14,11 @@ const routes: Routes =[
     path: '',
     component: AdminLayoutComponent,
     children: [
-        {
-      path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-  }]},
+      {
+        path: '',
+        loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+      }]
+  },
   {
     path: '**',
     redirectTo: 'dashboard'
--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,14 +6,10 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
 import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
 import { ChartsModule } from 'ng2-charts';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
 @NgModule({
   imports: [
@@ -28,12 +24,8 @@ import { UpgradeComponent } from '../../upgrade/upgrade.component';
     DashboardComponent,
     UserProfileComponent,
     TableListComponent,
-    UpgradeComponent,
-    TypographyComponent,
     IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
   ]
 })
 
-export class AdminLayoutModule {}
+export class AdminLayoutModule { }
